e86c42af6ddf9dfcf291816cee35bc1200efd38f
tranlebinh823 <tlbinh.a6.c3tqcap@gmail.com>
2023-09-25 11:51:51 +0700

    start-1

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -2,27 +2,6 @@
 @section('module','Category')
 @section('action','List')
 @section('content')
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Manage Categories</h2>
-        </div>
-        <div class="pull-right">
-            @can('category-create')
-            <a class="btn btn-success" href="{{ route('admin.categories.create') }}"> Create New Category</a>
-            @endcan
-        </div>
-    </div>
-</div>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
             <div class="row">
                 <div class="col-12">
@@ -74,43 +53,8 @@
                                 </tbody>
                             </table>
 
-<<<<<<< Updated upstream
                         </div> <!-- end card body-->
                     </div> <!-- end card -->
                 </div><!-- end col-->
             </div> <!-- end row-->
 @endsection
-=======
-
-<table class="table table-bordered">
-    <tr>
-        <th>No</th>
-        <th>Category Name</th>
-        <th width="280px">Action</th>
-    </tr>
-    @foreach ($categories as $category)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $category->category_name }}</td>
-        <td>
-            <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST">
-                <a class="btn btn-info" href="{{ route('admin.categories.show', $category->id) }}">Show</a>
-                @can('category-edit')
-                <a class="btn btn-primary" href="{{ route('admin.categories.edit', $category->id) }}">Edit</a>
-                @endcan
-
-                @csrf
-                @method('DELETE')
-                @can('category-delete')
-                <button type="submit" class="btn btn-danger">Delete</button>
-                @endcan
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
-
-{!! $categories->links() !!}
-
-@endsection
->>>>>>> Stashed changes
